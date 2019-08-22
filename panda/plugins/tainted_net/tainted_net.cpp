56203ebe20ab81dba03cc97120194b05b6806fed
Manolis Stamatogiannakis <mstamat@gmail.com>
2019-08-22 19:49:26 +0200

    Fixed some type errors related. The errors may or may not popup depending on your compiler. The network plugin should switch to targt_ptr_t for buffer addresses instead of overprovision by using uint64_t.

--- panda/plugins/tainted_net/tainted_net.cpp
+++ panda/plugins/tainted_net/tainted_net.cpp
@@ -120,7 +120,7 @@ static void output_message(const std::string &message)
 // User-specified options may limit the data that is tainted (e.g. only taint
 // bytes 56-60.)
 
-void taint_network_data(int packet_size, uint64_t old_buf_addr)
+void taint_network_data(int packet_size, target_ptr_t old_buf_addr)
 {
     // Counts number of labels applied to this packet.
     uint32_t num_labels_applied = 0;
@@ -204,8 +204,11 @@ static bool validate_ethertype(uint8_t *buf, int packet_size)
 }
 
 
-static void on_replay_handle_incoming_packet(CPUState *env, uint8_t *buf, int packet_size, uint64_t old_buf_addr)
+static void on_replay_handle_incoming_packet(CPUState *env, uint8_t *buf, int packet_size, uint64_t _old_buf_addr)
 {
+    // The interface always used uint64_t, irrespective of the guest address space.
+    // Downcast the address.
+    target_ptr_t old_buf_addr = (target_ptr_t)_old_buf_addr;
     assert(packet_size > 0);
     assert(buf);
     assert(old_buf_addr);
@@ -225,7 +228,7 @@ static void on_replay_handle_incoming_packet(CPUState *env, uint8_t *buf, int pa
     }
 }
 
-static void on_replay_handle_outgoing_packet(CPUState *env, uint8_t *buf, int packet_size, uint64_t old_buf_addr)
+static void on_replay_handle_outgoing_packet(CPUState *env, uint8_t *buf, int packet_size, target_ptr_t old_buf_addr)
 {
     if (0 != taint2_enabled())
     {
@@ -245,7 +248,7 @@ static void on_replay_handle_outgoing_packet(CPUState *env, uint8_t *buf, int pa
         fprintf(taintlogF, "\"Address\",\"Datum\",\"Labels\"\n");
 
         uint32_t numLabels = 0;
-        uint64_t curAddr = 0;
+        target_ptr_t curAddr = 0;
         for (int i = 0; i < packet_size; i++)
         {
             curAddr = old_buf_addr + i;
@@ -267,11 +270,11 @@ static void on_replay_handle_outgoing_packet(CPUState *env, uint8_t *buf, int pa
                 // characters
                 if (isprint(buf[i]))
                 {
-                    fprintf(taintlogF, "%" PRIu64 ",%c,", curAddr, buf[i]);
+                    fprintf(taintlogF, TARGET_PTR_FMT ",%c,", curAddr, buf[i]);
                 }
                 else
                 {
-                    fprintf(taintlogF, "%" PRIu64 ",.,", curAddr);
+                    fprintf(taintlogF, TARGET_PTR_FMT ",.,", curAddr);
                 }
                 for (int j = 0; j < numLabels; j++)
                 {
@@ -283,11 +286,11 @@ static void on_replay_handle_outgoing_packet(CPUState *env, uint8_t *buf, int pa
             {
                 if (isprint(buf[i]))
                 {
-                    fprintf(taintlogF, "%" PRIu64 ",%c, NULL\n", curAddr, buf[i]);
+                    fprintf(taintlogF, TARGET_PTR_FMT ",%c, NULL\n", curAddr, buf[i]);
                 }
                 else
                 {
-                    fprintf(taintlogF, "%" PRIu64 ",., NULL\n", curAddr);
+                    fprintf(taintlogF, TARGET_PTR_FMT ",., NULL\n", curAddr);
                 }
             }
         } // end of loop through items in TX buffer
@@ -303,7 +306,7 @@ static void on_replay_handle_outgoing_packet(CPUState *env, uint8_t *buf, int pa
 
 // a packet has come in over the network, or is about to go out over the network
 int on_replay_handle_packet(CPUState *env, uint8_t *buf, int packet_size, 
-        uint8_t direction, uint64_t old_buf_addr)
+        uint8_t direction, target_ptr_t old_buf_addr)
 {
     // Increment packet counter.  This count should agree with the count in the
     // wireshark file that is produced by the network plugin.
