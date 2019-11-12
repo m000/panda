556e04910df91cf6ea6f0d8dfc8f6970226d5de4
Manolis Stamatogiannakis <mstamat@gmail.com>
2019-11-12 18:21:19 +0100

    PANDA core: Added call for quiting the VM. Exposes the existing QMP interface.

--- panda/include/panda/rr/rr_api.h
+++ panda/include/panda/rr/rr_api.h
@@ -29,6 +29,7 @@
 #pragma once
 #include "panda/rr/rr_types.h"
 
+int panda_vm_quit(void);
 int panda_record_begin(const char *name, const char *snapshot);
 int panda_record_end(void);
 int panda_replay_begin(const char *name);
